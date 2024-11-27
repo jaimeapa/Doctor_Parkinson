ba292186550afbca49fa9c40da71bfe19990037a
pablorosalesrodriguez <pablo.rosalesrodriguez@usp.ceu.es>
2024-11-27 18:15:17 +0100

    module info al lobby

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.doctor_parkinson {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.logging;
-
-
-    opens com.example.doctor_parkinson to javafx.fxml;
-    exports com.example.doctor_parkinson;
-}
