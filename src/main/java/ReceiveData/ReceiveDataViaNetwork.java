991de60bf7c6ed6b11a6c154d28f90dd66a8f5da
Pablohitaa <pablo.hitalara@usp.ceu.es>
2024-12-05 17:16:29 +0100

    new receive and send data

--- src/main/java/ReceiveData/ReceiveDataViaNetwork.java
+++ src/main/java/ReceiveData/ReceiveDataViaNetwork.java
@@ -11,7 +11,7 @@ import java.util.logging.Logger;
 
 public class ReceiveDataViaNetwork {
 
-    public static String receiveString(DataInputStream dataInputStream) throws IOException {
+   /* public static String receiveString(DataInputStream dataInputStream) throws IOException {
         String information;
         information = dataInputStream.readUTF();
         return information;
@@ -21,7 +21,6 @@ public class ReceiveDataViaNetwork {
         Doctor doctor = null;
 
         try {
-            //Object tmp;
             int id = dataInputStream.readInt();
             String name = dataInputStream.readUTF();
             String surname = dataInputStream.readUTF();
@@ -103,5 +102,151 @@ public class ReceiveDataViaNetwork {
         }
 
         return interpretation;
-    }
-}
+    }*
+
+    */
+
+
+
+       public static String receiveString(Socket socket) throws IOException {
+
+           DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+           String information = "";
+           information = dataInputStream.readUTF();
+           releaseResources(dataInputStream);
+           return information;
+       }
+
+       public static Doctor receiveDoctor(Socket socket){
+           //InputStream inputStream = null;
+           //ObjectInputStream objectInputStream = null;
+           Doctor doctor = null;
+
+           try {
+               DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+               int id = dataInputStream.readInt();
+               String name = dataInputStream.readUTF();
+               String surname = dataInputStream.readUTF();
+               String date = dataInputStream.readUTF();
+               String email = dataInputStream.readUTF();
+               DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+               LocalDate dob = LocalDate.parse(date, formatter);
+               doctor = new Doctor(id,name,surname,dob,email);
+               releaseResources(dataInputStream);
+               //patient = (Patient) objectInputStream.readObject();
+           } catch (EOFException ex) {
+               System.out.println("All data have been correctly read.");
+           } catch (IOException  ex) {
+               System.out.println("Unable to read from the client.");
+               ex.printStackTrace();
+               //Logger.getLogger(ReceiveClientViaNetwork.class.getName()).log(Level.SEVERE, null, ex);
+           }
+
+           return doctor;
+       }
+
+       public static Patient recievePatient(Socket socket){
+           //InputStream inputStream = null;
+           //ObjectInputStream objectInputStream = null;
+           Patient patient = null;
+
+           try {
+               DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+               int id = dataInputStream.readInt();
+               String name = dataInputStream.readUTF();
+               String surname = dataInputStream.readUTF();
+               String date = dataInputStream.readUTF();
+               String email = dataInputStream.readUTF();
+               DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+               LocalDate dob = LocalDate.parse(date, formatter);
+               patient = new Patient(id,name,surname,dob,email);
+               releaseResources(dataInputStream);
+               //patient = (Patient) objectInputStream.readObject();
+           } catch (EOFException ex) {
+               System.out.println("All data have been correctly read.");
+           } catch (IOException  ex) {
+               System.out.println("Unable to read from the client.");
+               ex.printStackTrace();
+               //Logger.getLogger(ReceiveClientViaNetwork.class.getName()).log(Level.SEVERE, null, ex);
+           }
+
+           return patient;
+       }
+       public static Interpretation recieveInterpretation(Socket socket){
+           //InputStream inputStream = null;
+           //ObjectInputStream objectInputStream = null;
+           Interpretation interpretation = null;
+
+
+           try {
+               //Object tmp;
+               DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+               String stringDate = dataInputStream.readUTF();
+               int doctor_id = dataInputStream.readInt();
+               String stringEMG = dataInputStream.readUTF();
+               int patient_id = dataInputStream.readInt();
+               String stringEDA = dataInputStream.readUTF();
+               String observation = dataInputStream.readUTF();
+               String interpretation1 = dataInputStream.readUTF();
+               DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+               LocalDate date = LocalDate.parse(stringDate, formatter);
+               Signal signalEMG = new Signal(Signal.SignalType.EMG);
+               signalEMG.setValuesEMG(stringEMG);
+               Signal signalEDA = new Signal(Signal.SignalType.EDA);
+               signalEDA.setValuesEDA(stringEDA);
+               interpretation = new Interpretation(date, interpretation1, signalEMG, signalEDA, patient_id, doctor_id, observation);
+               releaseResources(dataInputStream);
+               //patient = (Patient) objectInputStream.readObject();
+           } catch (EOFException ex) {
+               System.out.println("All data have been correctly read.");
+           } catch (IOException  ex) {
+               System.out.println("Unable to read from the client.");
+               ex.printStackTrace();
+               //Logger.getLogger(ReceiveClientViaNetwork.class.getName()).log(Level.SEVERE, null, ex);
+           }
+
+           return interpretation;
+       }
+
+       public static int receiveInt(Socket socket) throws IOException{
+           //InputStream inputStream = socket.getInputStream();
+           //DataInputStream dataInputStream = new DataInputStream(inputStream);
+           int message = 10;
+           try{
+               DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+               message = dataInputStream.readInt();
+               releaseResources(dataInputStream);
+           }catch(IOException ex){
+               ex.printStackTrace();
+           }
+
+           return message;
+       }
+
+       public static User recieveUser(Socket socket)
+       {
+           User u = null;
+           try{
+               DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+               String email = dataInputStream.readUTF();
+               byte[] psw = dataInputStream.readUTF().getBytes();
+               String role = dataInputStream.readUTF();
+               Role r = new Role(role);
+               u = new User(email,psw,r);
+               releaseResources(dataInputStream);
+           }catch (IOException e){
+               e.printStackTrace();
+           }
+           return u;
+       }
+
+       private static void releaseResources(DataInputStream dataInputStream){
+           try {
+               dataInputStream.close();
+           } catch (IOException ex) {
+               //Logger.getLogger(SendBinaryDataViaNetwork.class.getName()).log(Level.SEVERE, null, ex);
+               ex.printStackTrace();
+           }
+       }
+
+   }
--- src/main/java/ReceiveData/SendDataViaNetwork.java
+++ src/main/java/ReceiveData/SendDataViaNetwork.java
@@ -1,6 +1,7 @@
 package ReceiveData;
 
 import Pojos.Doctor;
+import Pojos.Interpretation;
 import Pojos.Patient;
 import Pojos.User;
 
@@ -11,36 +12,104 @@ import java.util.logging.Logger;
 
 public class SendDataViaNetwork {
 
-    public static void sendStrings(String message, DataOutputStream dataOutputStream) throws IOException {
+    public static void sendStrings(String message, Socket socket) throws IOException {
+
+        //System.out.println("Connection established... sending text");
+        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
         dataOutputStream.writeUTF(message);
+        releaseResources(dataOutputStream);
+        //releaseResourcesForString(printWriter,socket);
+
     }
-    public static void sendInt(Integer message,  DataOutputStream dataOutputStream) {
+    public static void sendInt(Integer message,  Socket socket) throws IOException{
+        //OutputStream outputStream = socket.getOutputStream();
+        //DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
         try{
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
             dataOutputStream.writeInt(message);
+            releaseResources(dataOutputStream);
         }catch (IOException ex){
             ex.printStackTrace();
         }
+
+        //releaseResourcesInt(dataOutputStream,outputStream);
     }
 
-    public static void sendDoctor(Doctor doctor, DataOutputStream dataOutputStream)
+
+    public static void sendPatient(Patient patient, Socket socket)
     {
+        //OutputStream outputStream = null;
+        //ObjectOutputStream objectOutputStream = null;
+
+        /*try {
+            //socket = new Socket("localhost", 8080);
+            outputStream = socket.getOutputStream();
+        } catch (IOException ex) {
+            System.out.println("It was not possible to connect to the server.");
+            System.exit(-1);
+            Logger.getLogger(SendDataViaNetwork.class.getName()).log(Level.SEVERE, null, ex);
+        }*/
+        try {
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            dataOutputStream.writeInt(patient.getPatient_id());
+            dataOutputStream.writeUTF(patient.getName());
+            dataOutputStream.writeUTF(patient.getSurname());
+            dataOutputStream.writeUTF(patient.getDob().toString());
+            dataOutputStream.writeUTF(patient.getEmail());
+            releaseResources(dataOutputStream);
+        } catch (IOException ex) {
+            System.out.println("Unable to write the objects on the server.");
+            Logger.getLogger(SendDataViaNetwork.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public static void sendInterpretation(Interpretation interpretation, Socket socket) throws IOException{
+        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+        dataOutputStream.writeUTF(interpretation.getDate().toString());
+        dataOutputStream.writeInt(interpretation.getDoctor_id());
+        dataOutputStream.writeUTF(interpretation.getSignalEMG().valuesToString());
+        dataOutputStream.writeInt(interpretation.getPatient_id());
+        dataOutputStream.writeUTF(interpretation.getSignalEDA().valuesToString());
+        dataOutputStream.writeUTF(interpretation.getObservation());
+        dataOutputStream.writeUTF(interpretation.getInterpretation());
+        releaseResources(dataOutputStream);
+    }
+    public static void sendUser(User u, Socket socket) throws IOException
+    {
+        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+        dataOutputStream.writeUTF(u.getEmail());
+        byte[] password = u.getPassword();
+
+        dataOutputStream.writeUTF(new String(password));
+        dataOutputStream.writeUTF(u.getRole().toString());
+        releaseResources(dataOutputStream);
+    }
+    public static void sendDoctor(Doctor doctor, Socket socket) throws IOException {
+        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
         try {
             dataOutputStream.writeInt(doctor.getDoctor_id());
             dataOutputStream.writeUTF(doctor.getName());
             dataOutputStream.writeUTF(doctor.getSurname());
             dataOutputStream.writeUTF(doctor.getDob().toString());
             dataOutputStream.writeUTF(doctor.getEmail());
-
+            releaseResources(dataOutputStream);
         } catch (IOException ex) {
             System.out.println("Unable to write the objects on the server.");
             Logger.getLogger(SendDataViaNetwork.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    public static void sendUser(User u, DataOutputStream dataOutputStream) throws IOException
-    {
-        dataOutputStream.writeUTF(u.getEmail());
-        dataOutputStream.writeUTF(new String(u.getPassword()));
-        dataOutputStream.writeUTF(u.getRole().toString());
+
+
+
+
+    private static void releaseResources(DataOutputStream dataOutputStream){
+        try {
+            dataOutputStream.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
+
 }
